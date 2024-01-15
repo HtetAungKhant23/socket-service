91df782c2b2ab772aaf911cf941ce9defa49200b
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-15 16:47:54 +0630

    add: custom socket.io adapter using redis for sharing real-time events amoung multiple sever

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
+
+  await app.listen(3004);
 }
 bootstrap();
